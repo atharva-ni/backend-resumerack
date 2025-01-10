f1843bc2d7d69f6e746ec3e23ea812e45c62875f
Atharva <atharva.nighto30@gmail.com>
2025-01-10 16:12:21 +0530

    add / get route at server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json()); // To handle JSON data in requests
 // Routes
 app.use("/api", fileUploadController);
 
+app.get("/", (req, res) => {
+    res.send("Hello atharva from backend")
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
